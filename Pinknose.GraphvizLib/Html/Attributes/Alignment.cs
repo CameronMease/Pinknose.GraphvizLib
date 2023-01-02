75e86f134c01d37405dbe34bcd61b0bb4ce4eb6e
Cameron Mease <cameron@pinknose.net>
2023-01-02 11:18:52 -0500

    Ran codemaid.

--- Pinknose.GraphvizLib/Html/Attributes/Alignment.cs
+++ Pinknose.GraphvizLib/Html/Attributes/Alignment.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pinknose.GraphvizLib.Html.Attributes
+﻿namespace Pinknose.GraphvizLib.Html.Attributes
 {
     public class Alignment : HtmlAttribute, ITableAttribute, ICellAttribute
     {
-        public override string Name => "ALIGN";
+        #region Properties
 
         public static Alignment Center => new Alignment() { Value = "CENTER" };
         public static Alignment Left => new Alignment() { Value = "LEFT" };
         public static Alignment Right => new Alignment() { Value = "RIGHT" };
+        public override string Name => "ALIGN";
+
+        #endregion Properties
     }
 }
